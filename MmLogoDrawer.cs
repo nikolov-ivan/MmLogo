2b586ad4fdc0943af32d34fdb4f7fc455e06bab2
ivan nikolov <nikolov.iv@gmail.com>
2020-02-23 18:36:51 +0200

    Upload

---
+++ MmLogoDrawer.cs
@@ -0,0 +1,70 @@
+﻿namespace MmLogo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class MmLogoDrawer
+    {
+        private char filledSpace = '*';
+        private char freeSpace = '-';
+        private const int numberOfMDraw = 2;
+        public string Draw(int width)
+        {
+            StringBuilder sb = new StringBuilder();
+            this.DrawTopPart(sb, width);
+            this.DrawBottomPart(sb, width);
+            return sb.ToString();
+        }
+
+        private void DrawTopPart(StringBuilder sb, int width)
+        {
+            for (int rowNumber = 0; rowNumber < width - rowNumber; rowNumber++)
+            {
+                this.DrawTopMm(sb, width, rowNumber);
+
+            }
+
+        }
+
+        private void DrawTopMm(StringBuilder sb, int width, int rowNumber)
+        {
+            for (int i = 0; i < numberOfMDraw; i++)
+            {
+                sb.Append(this.freeSpace, width - rowNumber);
+                sb.Append(this.filledSpace, width + 2 * rowNumber);
+                sb.Append(this.freeSpace, width - 2 * rowNumber);
+                sb.Append(this.filledSpace, width + 2 * rowNumber);
+                sb.Append(this.freeSpace, width - rowNumber);
+            }
+            sb.AppendLine();
+
+        }
+
+
+        private void DrawBottomPart(StringBuilder sb, int width)
+        {
+            for (int i = 0; i < width - i; i++)
+            {
+                this.DrawBottomMm(sb, width, i);
+                
+                sb.AppendLine();
+            }
+        }
+
+        private void DrawBottomMm(StringBuilder sb, int width, int rowNumber)
+        {
+            for (int i = 0; i < numberOfMDraw; i++)
+            {
+                sb.Append(this.freeSpace, width / 2 - rowNumber);
+                sb.Append(this.filledSpace, width);
+                sb.Append(this.freeSpace, 1 + 2 * rowNumber);
+                sb.Append(this.filledSpace, 2 * width - 1 - 2 * rowNumber);
+                sb.Append(this.freeSpace, 1 + 2 * rowNumber);
+                sb.Append(this.filledSpace, width);
+                sb.Append(this.freeSpace, width / 2 - rowNumber);
+            }
+            
+        }
+
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace MmLogo
+{
+    using System;
+    using System.Text;
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (!IsValidInput(input))
+                {
+                    Console.WriteLine("Please input a odd number between 2 and 10 000");
+                    continue;
+                }
+                int width = int.Parse(input);
+                var logo = new MmLogoDrawer();
+                var resultLogo = logo.Draw(width);
+                Console.WriteLine(resultLogo);
+                break;
+            }
+
+        }
+        public static bool IsValidInput(string input)
+        {
+            int number;
+            bool isNumber = int.TryParse(input, out number);
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            if (!isNumber)
+            {
+                return false;
+            }
+            if (number < 3 || number > 9999)
+            {
+                return false;
+            }
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
